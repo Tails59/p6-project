6b507faed76ec307ab467d8b8676bf824db1e2a5
Davide Kovac <47418599+DavideKovac@users.noreply.github.com>
2021-02-17 13:22:44 +0000

    Updating Controller 1 name
    Updating Controller name to add the Title dynamically

--- MVCExample-starter-files/mvcexample/Controller1.java
+++ MVCExample-starter-files/mvcexample/Controller1.java
@@ -22,15 +22,16 @@ public class Controller1 extends JFrame
     private JButton clearViews;  // For direct message to views
     private JButton incA;        // To prompt the model to "modify" itself (A component)
     private JButton quit;        // As it says
+    String modelName
     
     // Constructor
-    public Controller1(Model model) {
+    public Controller1(Model model,String modelName) {
         
         // Record reference to the model
         this.model = model;
-        
+        this.modelName=modelName;
         // Configure the window
-        setTitle("Controller1");
+        setTitle(modelName);
         setLocation(40,40);
         setSize(350,150);
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
