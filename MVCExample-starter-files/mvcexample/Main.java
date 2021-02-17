48ceff60208639483520fbccebf873014da947e2
almantas42 <79205344+almantas42@users.noreply.github.com>
2021-02-17 13:33:31 +0000

    Update Main.java
    Creating additional controllers and addint int for Y position on the screen.

--- MVCExample-starter-files/mvcexample/Main.java
+++ MVCExample-starter-files/mvcexample/Main.java
@@ -12,7 +12,9 @@ public class Main {
      
         Model model = new Model();               // Create shared database
         Controller1 c1 = new Controller1(model,"Controller 1"); // Create first controller
-        Controller2 c2 = new Controller2(model,"Controller 2"); // Create second controller
+        Controller2 c2 = new Controller2(model, "Controller 2", 0); // Create second controller
+        Controller2 c2a = new Controller2(model, "Controller 3", 1); // Create third controller
+        Controller2 c2b = new Controller2(model, "Controller 4", 2); // Create fourth controller
         
     } // main
   
