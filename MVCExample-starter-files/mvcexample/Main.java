4ead3748c534f340578003b5a36123fb6346296f
Davide Kovac <47418599+DavideKovac@users.noreply.github.com>
2021-02-17 13:24:52 +0000

    Update the name of the objects

--- MVCExample-starter-files/mvcexample/Main.java
+++ MVCExample-starter-files/mvcexample/Main.java
@@ -11,8 +11,8 @@ public class Main {
     public static void main(String args[]) {
      
         Model model = new Model();               // Create shared database
-        Controller1 c1 = new Controller1(model); // Create first controller
-        Controller2 c2 = new Controller2(model); // Create second controller
+        Controller1 c1 = new Controller1(model,"Controller 1"); // Create first controller
+        Controller2 c2 = new Controller2(model,"Controller 2"); // Create second controller
         
     } // main
   
